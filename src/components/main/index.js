4325b8aeea155e1062a753e66ca2cee29d9305d8
Adam Handermann <adamhandermann@gmail.com>
2018-10-27 15:19:42 -0700

    Bug Fix

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Layout from '../common/layout';
+import Layout from "../common/layout";
 
 class Main extends Component {
   render() {
@@ -9,26 +9,50 @@ class Main extends Component {
         <div className="hello">
           <h1 className="bold">HI. I'M ADAM</h1>
           <div className="sub">
-            <h2 className="subhead">I AM CURRENTLY <span>ACCEPTING</span> FREELANCE WORK</h2>
+            <h2 className="subhead">
+              I AM A <span>BRAND AND WEB</span> DESIGNER
+            </h2>
           </div>
         </div>
         <div className="work">
           <div className="work-row">
-            <Link to="/portfolio"> <div className="work-image work-hover trail"></div> </Link>
-            <Link to="/portfolio"> <div className="work-image work-hover vans"></div> </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover trail" />{" "}
+            </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover vans" />{" "}
+            </Link>
           </div>
           <div className="work-row">
-            <Link to="/portfolio"> <div className="work-image work-hover guitar"></div> </Link>
-            <Link to="/portfolio"> <div className="work-image work-hover cotton"></div> </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover guitar" />{" "}
+            </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover cotton" />{" "}
+            </Link>
           </div>
           <div className="work-row">
-            <Link to="/portfolio"> <div className="work-image work-hover local"></div> </Link>
-            <Link to="/portfolio"> <div className="work-image work-hover white"></div> </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover local" />{" "}
+            </Link>
+            <Link to="/portfolio">
+              {" "}
+              <div className="work-image work-hover white" />{" "}
+            </Link>
           </div>
         </div>
         <div className="contact">
           <h1>LET'S CONNECT</h1>
-          <p> HAVE A PROJECT YOU WANT TO WORK ON, OR JUST WANT TO GRAB COFFEE? FEEL FREE TO GET IN TOUCH.</p>
+          <p>
+            {" "}
+            HAVE A PROJECT YOU WANT TO WORK ON, OR JUST WANT TO GRAB COFFEE?
+            FEEL FREE TO GET IN TOUCH.
+          </p>
           <h2>hello@damnhand.com</h2>
         </div>
       </Layout>
